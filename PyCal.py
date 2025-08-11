92428dc67e650908aa1c28198417af6e31190dff
luts <ravelolutherl@gmail.com>
2025-08-11 15:25:50 +0800

    kahit ano

---
+++ PyCal.py
@@ -0,0 +1,22 @@
+# Simple Python Calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4.
